54ffb5b368938d01a237a1c67b925ca67c4e9d66
nerdelt <nerdelt@gmail.com>
2018-10-11 11:19:54 -0500

    Class Enum Assignment

---
+++ ClassEnum/ClassEnum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+
+namespace ClassEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Below are the first names of all the students in a class: \n");
+
+            //create new student class object
+            Student student = new Student();
+
+            //lets user see names to choose from
+            student.PrintClassList();
+
+            Console.Write("\nPlease type in a first name to see the student's last name: ");
+
+            //takes user input and creates string
+            string nameEntered = Console.ReadLine();
+
+            //parses users input to be used in case/switch
+            Student.FirstName firstName = (Student.FirstName)Enum.Parse(typeof(Student.FirstName), nameEntered);
+
+            //prints student's full name to console
+            Console.Write($"\nThe student's full name is: {nameEntered} ");
+
+            student.LastName(firstName);
+
+            Console.ReadLine();
+
+        }    
+     }
+  }
+
---
+++ ClassEnum/ClassEnum/Student.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ClassEnum
+{
+    class Student
+    {
+        //enum of all student's first names
+        public enum FirstName
+        {
+
+            Marjorie,
+            Howard,
+            Norma,
+            Ruby,
+            Joyce,
+            Francis,
+            Melvin,
+            Edith,
+            Clyde,
+            Agnes,
+            Gilbert,
+            Clara,
+            Russell,
+            Florence
+        }
+
+        //prints all students first names
+        public void PrintClassList()
+        {
+            foreach (FirstName name in Enum.GetValues(typeof(FirstName)))
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        //case/switch thats gives user student's last name when first name is entered.
+        public void LastName(FirstName firstname)
+        {
+            switch (firstname)
+            {
+                case FirstName.Marjorie:
+                    Console.WriteLine("Boucher");
+                    break;
+
+                case FirstName.Howard:
+                    Console.WriteLine("Newell");
+                    break;
+
+                case FirstName.Norma:
+                    Console.WriteLine("Childs");
+                    break;
+
+                case FirstName.Ruby:
+                    Console.WriteLine("Ibarra");
+                    break;
+
+                case FirstName.Joyce:
+                    Console.WriteLine("Winter");
+                    break;
+
+                case FirstName.Francis:
+                    Console.WriteLine("Davies");
+                    break;
+
+                case FirstName.Melvin:
+                    Console.WriteLine("Schultz");
+                    break;
+
+                case FirstName.Edith:
+                    Console.WriteLine("Gibbons");
+                    break;
+
+                case FirstName.Clyde:
+                    Console.WriteLine("Westridge");
+                    break;
+
+                case FirstName.Agnes:
+                    Console.WriteLine("Delarosa");
+                    break;
+
+                case FirstName.Gilbert:
+                    Console.WriteLine("Kessler");
+                    break;
+
+                case FirstName.Clara:
+                    Console.WriteLine("Downey");
+                    break;
+
+                case FirstName.Russell:
+                    Console.WriteLine("Espinosa");
+                    break;
+
+                case FirstName.Florence:
+                    Console.WriteLine("Vogel");
+                    break;
+
+                default:
+                    Console.WriteLine("We're sorry, but that name is not in the class list.");
+                    break;
+
+            }
+           
+        }
+    }
+}
